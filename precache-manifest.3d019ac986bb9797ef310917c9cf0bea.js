c49173ff40d948d952514e06234744932ef13eb6
junbucks7 <juney214@gmail.com>
2020-03-01 09:09:41 +0900

    second commit

---
+++ precache-manifest.3d019ac986bb9797ef310917c9cf0bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640cb11f7150e36a4d82e8242196faf1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0416130b382f50e2cd01",
+    "url": "./static/css/main.79a8a3da.chunk.css"
+  },
+  {
+    "revision": "e88e92a66f21a545bc0f",
+    "url": "./static/js/2.3ee55f50.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.3ee55f50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0416130b382f50e2cd01",
+    "url": "./static/js/main.483954c0.chunk.js"
+  },
+  {
+    "revision": "44a06ee2722ae10c6778",
+    "url": "./static/js/runtime-main.97a7e1ab.js"
+  }
+]);
